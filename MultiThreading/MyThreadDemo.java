9e247375b8a1214f5748f80b78d69c45826fc96c
Aanchal Gautam <Aanchalgautam759@gmail.com>
2025-05-26 23:27:42 +0530

    Creating Threads by Extending Thread Class

---
+++ MultiThreading/MyThreadDemo.java
@@ -0,0 +1,31 @@
+
+
+class MyThread extends Thread {
+    private String threadName;
+
+    MyThread(String name) {
+        threadName = name;
+    }
+
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(threadName + " - Count: " + i);
+            try {
+                Thread.sleep(500); // sleep for 500ms
+            } catch (InterruptedException e) {
+                System.out.println(threadName + " interrupted.");
+            }
+        }
+        System.out.println(threadName + " finished.");
+    }
+}
+
+public class MyThreadDemo {
+    public static void main(String[] args) {
+        MyThread t1 = new MyThread("Thread-1");
+        MyThread t2 = new MyThread("Thread-2");
+
+        t1.start(); // starts the first thread
+        t2.start(); // starts the second thread
+    }
+}
